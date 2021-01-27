385ca0c7e9368797db10d6a0519fdaab8015b687
sakshi-khatri <sakshi@fusemachines.com>
2021-01-27 14:41:01 +0545

    First commit

---
+++ src/main/java/com/example/jenkinsTest/controller/TestController.java
@@ -0,0 +1,20 @@
+
+package com.example.jenkinsTest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Sakshi Khatri
+ *
+ */
+@RestController
+@RequestMapping("/")
+public class TestController {
+	@GetMapping
+	public String test() {
+		return "Test pass";
+	}
+
+}
